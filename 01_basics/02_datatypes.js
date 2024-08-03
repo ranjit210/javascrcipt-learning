ed5a88050082ae1a052240cf44a697634f71dbac
ranjit210 <73810593+ranjit210@users.noreply.github.com>
2024-08-03 11:14:38 +0000

    changes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+
+"use strict" // treat all js code as newer version
+
+// alert(3+3) // we are using node js, not browser
+
+let name = "ranjit"
+let age =19
+let isLoggedIn =false
+
+// number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/fale
+//null =>standlone value / object
+//undefined => undefined
+//symbol => unique
+
+
