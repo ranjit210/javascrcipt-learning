9e021610f8b37dbc73be362bf8e41596b02e642e
ranjit210 <73810593+ranjit210@users.noreply.github.com>
2024-08-04 02:53:59 +0000

    data conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,29 @@
+let score ="33sd"
+let score1 ="undefined"
+let score3 ="null"
+let score4="true"
+
+let cNUm =Number(score)
+
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+console.log(typeof cNUm)  //this will give number
+console.log(cNUm)   // but this will pring NaN
+console.log(score1) // it will return NaN
+console.log(score3); // it will return zero
+console.log(score4);// it will return 1
+
+
+
+
+
+// "33" =>33
+//"33abc" =>NaN
+//true =>1; false =>0
+
+//1 =>true; 0 =>false
+//""=>false   empty string return false
+
+//"ranjit" =>true 
