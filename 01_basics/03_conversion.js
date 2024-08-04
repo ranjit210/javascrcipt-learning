5f915ff42abffd9e4bf5406ff298b6e93051e0c8
ranjit210 <73810593+ranjit210@users.noreply.github.com>
2024-08-04 06:10:24 +0000

    update

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -3,17 +3,17 @@ let score1 ="undefined"
 let score3 ="null"
 let score4="true"
 
-let cNUm =Number(score)
+//let cNUm =Number(score)
 
-let booleanIsLoggedIn =Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//let booleanIsLoggedIn =Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 
-console.log(typeof cNUm)  //this will give number
-console.log(cNUm)   // but this will pring NaN
-console.log(score1) // it will return NaN
-console.log(score3); // it will return zero
-console.log(score4);// it will return 1
+//console.log(typeof cNUm)  //this will give number
+//console.log(cNUm)   // but this will pring NaN
+//console.log(score1) // it will return NaN
+//console.log(score3); // it will return zero
+//console.log(score4);// it will return 1
 
 
 
@@ -27,3 +27,14 @@ console.log(score4);// it will return 1
 //""=>false   empty string return false
 
 //"ranjit" =>true 
+
+//...........operations...............
+let a = 4
+let b =++a
+console.log(a)
+console.log(b)
+console.log("....break....")
+let y =4
+let z=y++
+console.log(y)
+console.log(z)
