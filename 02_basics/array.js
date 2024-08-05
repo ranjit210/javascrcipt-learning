2d95b81718243fc53dc3178159d183f8e466aa53
ranjit210 <73810593+ranjit210@users.noreply.github.com>
2024-08-05 11:34:11 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,14 @@
+const myArray =[0,1,2,3,4,5]
+//console.log( myArray)
+//console.log(myArray[1])
+//const myArray2=new Array(0,1,2,3,4,5)
+//console.log(myArray2)
+
+//.....Array Methods.....
+//myArray.push(6)   // add value at the end of the array
+//console.log(myArray)
+//myArray.pop()   //remove last element of the  array
+console.log(myArray)
+//myArray.unshift(9) //add first element in the array
+
+console.log(myArray)
