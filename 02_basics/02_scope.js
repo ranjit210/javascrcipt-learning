c2f64653330971b00af5ba677b025b6363add8a4
ranjit210 <73810593+ranjit210@users.noreply.github.com>
2024-08-11 07:12:41 +0000

    updated

---
+++ 02_basics/02_scope.js
@@ -0,0 +1,12 @@
+function one(){
+    const userName ="ranjit"
+    function two(){
+        const website="youtune"
+        console.log(userName)
+
+    }
+    console.log(website)
+    two()
+}
+
+one()
