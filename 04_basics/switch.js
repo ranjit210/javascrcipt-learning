c68a0b55ecd3a0ad459d147d674531d67de2bec1
ranjit210 <73810593+ranjit210@users.noreply.github.com>
2024-08-13 07:24:00 +0000

    switch

---
+++ 04_basics/switch.js
@@ -0,0 +1,31 @@
+// switch
+
+const month =9;
+
+switch (month) {
+    case 1:
+        console.log("january")
+        
+        break;
+        
+    case 2:
+        console.log("feb")
+        
+        break;
+        
+    case 3:
+        console.log("mar")
+        
+        break;
+    case 4:
+        console.log("april")
+        
+        break;
+        
+    
+        
+
+    default:
+        console.log("default")
+        break;
+}
