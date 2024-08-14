023cf74d58bb0875d4b91eb2fd166547f42b488f
ranjit210 <73810593+ranjit210@users.noreply.github.com>
2024-08-14 12:04:06 +0000

    loop

---
+++ 05_iterations/01_iteration.js
@@ -0,0 +1,27 @@
+// for loop
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    if(element ===5){
+        //console.log('5 is the best number')
+    }
+    //console.log(element)  
+}
+
+
+for (let i = 1; i <=10; i++) {
+    //console.log(`outer loop ${i}`)
+    for (let j = 1; j<=10; j++) {
+        //console.log(`inner loop ${j}`)
+        //console.log(`${i}*${j}= ${i*j}`)
+    }
+
+    
+}
+let arr=["ramesh","pawan","lalu"]
+
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element)
+    
+}
