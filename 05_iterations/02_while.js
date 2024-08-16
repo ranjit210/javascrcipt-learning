f9e6aa4aa4bf2dd7d2007ac9751989102b466001
ranjit210 <73810593+ranjit210@users.noreply.github.com>
2024-08-16 04:57:18 +0000

    while

--- 05_iterations/02_while.js
+++ 05_iterations/02_while.js
@@ -1,5 +1,31 @@
 for (let index = 100; index <= 1; index--) {
     const element = index;
-    console.log(element)
+   // console.log(element)
     
 }
+
+let i =1
+while(i<20){
+    //console.log(`print i :${i}`)
+    i=i*2;
+
+}
+
+//arrray
+
+let arr =[1,2,3,4,5]
+let ar=0
+while(ar<arr.length){
+   // console.log(arr[ar])
+    ar=ar+1
+}
+
+//do...while loop
+let score =15
+do {
+    console.log(`score is ${score}`)
+    score ++
+    
+
+    
+} while (score<=10);
